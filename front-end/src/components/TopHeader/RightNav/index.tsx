863b25595f4a62d3d683064c13b0b97d301abbc2
Creekyu <xby503094716@gmail.com>
2023-04-18 12:25:04 +0800

    fix

--- front-end/src/components/TopHeader/RightNav/index.tsx
+++ front-end/src/components/TopHeader/RightNav/index.tsx
@@ -3,19 +3,18 @@ import React from 'react';
 // css
 import './index.scss';
 
-// antd
-import { Avatar } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
-
 // ui-c
 import LinkBtn2 from '@/components/UI/LinkBtn2';
+
+// img
+import img from '@/assets/images/left-nav-icon.png';
 
 const RightNav = () => {
   return (
     <div className="right-nav">
       <LinkBtn2>Sing up</LinkBtn2>
       <LinkBtn2>Sign in</LinkBtn2>
-      <Avatar className="avator-box" icon={<UserOutlined />} />
+      <div className="right-nav-avator" style={{ backgroundImage: `url(${img})` }}></div>
     </div>
   );
 };
