08bd7e989f5b676b307ab7fa81149e931b55435e
Creekyu <xby503094716@gmail.com>
2023-04-18 12:01:06 +0800

    fix

--- front-end/src/pages/MainPage/HomePage/index.tsx
+++ front-end/src/pages/MainPage/HomePage/index.tsx
@@ -3,13 +3,17 @@ import React from 'react';
 // css
 import './index.scss';
 
+// img
+import img1 from '@/assets/images/home.jpg';
+import img2 from '@/assets/images/left-nav-icon.png';
+
 const HomePage = () => {
   return (
     <>
       <div className="back-white"></div>
-      <div className="background-photo">
+      <div className="background-photo" style={{ backgroundImage: `url(${img1})` }}>
         <div className="home-tag-wrapper">
-          <div className="home-tag-icon"></div>
+          <div className="home-tag-icon" style={{ backgroundImage: `url(${img2})` }}></div>
           <div className="home-tag">Criik-Blog</div>
           <div className="home-allegory">Always Be Yourself and Never Compromise to the Life</div>
         </div>
